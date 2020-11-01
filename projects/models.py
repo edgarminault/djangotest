be5ac9468b750c3b5b8c149517a594a76647d146
edgar minault <e.minault@bluewin.ch>
2020-11-01 13:08:53 +0100

    Docstring of model Project is defined.

--- projects/models.py
+++ projects/models.py
@@ -1,9 +1,15 @@
 from django.db import models
 
+
 class Project(models.Model):
-    """docstring for Project"""
+    """
+    OUTPUT:
+    --------------------
+    A project has a title, description, technology and image. Hence, cards can be built
+    when the model is initialised.
+    """
+
     title = models.CharField(max_length=100)
     description = models.TextField()
     technology = models.CharField(max_length=20)
     image = models.FilePathField(path="/img")
-
