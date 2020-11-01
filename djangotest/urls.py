39c4b1e45fb4b140bdc831ac88e76913062b3200
edgar minault <e.minault@bluewin.ch>
2020-11-01 16:41:19 +0100

    User Management.

--- djangotest/urls.py
+++ djangotest/urls.py
@@ -13,11 +13,21 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+# from django.contrib import admin
+# from django.urls import path, include
+# from django.conf.urls import include as include_url, url
+
+# urlpatterns = [
+#     url(r"^", include_url("users.urls")),
+#     url(r"^admin/", admin.site.urls),
+#     path("projects/", include("projects.urls")),
+#     path("blog/", include("blog.urls")),
+# ]
+
+from django.conf.urls import include, url
 from django.contrib import admin
-from django.urls import path, include
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("projects/", include("projects.urls")),
-    path("blog/", include("blog.urls")),
+    url(r"^", include("users.urls")),
+    url(r"^admin/", admin.site.urls),
 ]
