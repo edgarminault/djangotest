7fe79ae94e7497d53b646f5f34b8b7806bdbce30
edgar minault <e.minault@bluewin.ch>
2020-11-01 14:35:10 +0100

    New views created and images added.

--- projects/views.py
+++ projects/views.py
@@ -3,6 +3,23 @@ from projects.models import Project
 
 
 def project_index(request):
+    """
+    PROJECT INDEX Page:
+    -----------------------
+    Returns all projects in a cards layout.
+    """
     projects = Project.objects.all()
     context = {"projects": projects}
     return render(request, "project_index.html", context)
+
+def project_detail(request, pk):
+    """
+    PROJECT DETAIL Page:
+    Returns a detailed page of a project. Is an output page when clicking on a project
+    in the index page.
+    """
+    project = Project.objects.get(pk=pk)
+    context = {
+        'project': project
+    }
+    return render(request, 'project_detail.html', context)
